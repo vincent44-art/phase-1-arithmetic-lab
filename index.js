12504837e70b37603b56dd319382612f23f651c0
vincent44-art <vincent.murithi@student.moringaschoo.com>
2025-03-11 11:21:07 +0300

    commit

--- index.js
+++ index.js
@@ -1,2 +1,27 @@
 
 // Write your code here
+
+// Write your code here
+let num1=31;
+let num2=2;
+const multiply =(31*2)
+console.log(multiply)
+
+
+let random=1;
+random++
+random;
+
+
+let num3=12;
+let num4=3;
+const mod=(num3/num4)
+console.log(mod)
+
+let max=20
+while (i <= 20){
+    console.log(max)
+    max++
+}
+
+
