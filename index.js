5f7500253b0930fa831958874ff6268ec5a49372
vincent44-art <vincent.murithi@student.moringaschoo.com>
2025-03-22 11:31:43 +0300

    commit

--- index.js
+++ index.js
@@ -23,5 +23,20 @@ while (i <= 20){
     console.log(max)
     max++
 }
+function hasTargetSum(array, target) {
+    for (let i = 0; i < array.length; i++) {
+        for (let j = i + 1; j < array.length; j++) {
+            if (array[i] + array[j] === target) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+// Test cases
+console.log(hasTargetSum([3, 8, 12, 4, 11, 7], 10)); // true
+console.log(hasTargetSum([22, 19, 4, 6, 30], 25)); // true
+console.log(hasTargetSum([1, 2, 5], 4)); // false
 
 
